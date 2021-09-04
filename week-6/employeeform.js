a7a259a0da6d3f566dd04d0d2fcdaa14ba3abd1f
jdhoye <77636767+jdhoye@users.noreply.github.com>
2021-09-04 09:49:57 -0400

    Add files via upload

--- week-6/employeeform.js
+++ week-6/employeeform.js
@@ -11,19 +11,19 @@ Employees.prototype.printEmployeeForm = function() {
     console.log("Job Title: " + this.title);
     console.log("Salary: " + this.salary);
     console.log("FT/PT: " + this.status);
-    console.log("Is this a new Employee?: " + this.isNew);
     console.log("================================");
 }
-
-Employees.prototype.isNew = true;
 
 
 var myEmployee = new Employees("Jeremiah", "Ops Associate", "$14/hr");
 myEmployee.printEmployeeForm();
 
 var myEmployee1 = new Employees("Michael", "Manager", "$19/hr");
-myEmployee1.isNew = false
+myEmployee1.status = "Contract";
 myEmployee1.printEmployeeForm();
 
 var myEmployee2 = new Employees("Vanessa", "Admin Asst.", "$17/hr", "Part-Time");
 myEmployee2.printEmployeeForm();
+
+Employee.push(myEmployee, myEmployee1, myEmployee2);
+console.log(Employee.length);
